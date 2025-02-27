bb55b93061611708d2a91cdabea889de25d4facc
Patrick Davlin <patrick.davlin@sportstechinc.com>
2025-02-27 14:11:49 -0600

    feat(vim): update plugins to reduce lag

--- dotfiles/config/nvim/lua/pdav/plugins/lsp/init.lua
+++ dotfiles/config/nvim/lua/pdav/plugins/lsp/init.lua
@@ -1,63 +1,94 @@
 local formatters = {
-    javascript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    astro = { "prettier" },
-    json = { "prettier" },
-    jsonc = { "prettier" },
-    html = { "prettier" },
-    yaml = { "prettier" },
-    css = { "stylelint", "prettier" },
-    sh = { "shellcheck", "shfmt" },
-    lua = { "stylua" },
+  javascript = { "prettier" },
+  javascriptreact = { "prettier" },
+  typescript = { "prettier" },
+  typescriptreact = { "prettier" },
+  astro = { "prettier" },
+  json = { "prettier" },
+  jsonc = { "prettier" },
+  html = { "prettier" },
+  yaml = { "prettier" },
+  css = { "stylelint", "prettier" },
+  sh = { "shellcheck", "shfmt" },
+  lua = { "stylua" },
+  ruby = { "rubocop" },
+  php = { "pint" },
 }
 
 return {
-    {
-        "liuchengxu/vista.vim",
-        lazy = true,
-        cmd = "Vista",
-        config = function()
-            vim.g.vista_default_executive = "nvim_lsp"
-        end,
+  {
+    "liuchengxu/vista.vim",
+    lazy = true,
+    cmd = "Vista",
+    cond = not vim.g.vscode,
+    config = function()
+      vim.g.vista_default_executive = "nvim_lsp"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    cond = not vim.g.vscode,
+    dependencies = {
+      -- Helpers to install LSPs and maintain them
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp",
     },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        dependencies = {
-            -- Helpers to install LSPs and maintain them
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            require("pdav.plugins.lsp.config").setup()
-        end,
+    config = function()
+      require("pdav.plugins.lsp.config").setup()
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_fallback = false,
+      },
+      formatters_by_ft = formatters,
     },
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            format_on_save = {
-                timeout_ms = 2000,
-                lsp_fallback = false,
-            },
-            formatters_by_ft = formatters,
-        },
-    },
+  },
 
-    {
-        "folke/trouble.nvim",
-        config = true,
-        keys = {
-            { "<leader>xx", "<cmd>TroubleToggle<cr>" },
-            { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-            { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
-            { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
-            { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
-        },
+  {
+    "folke/trouble.nvim",
+    config = true,
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     },
-    {
-        "vuki656/package-info.nvim",
-        config = true,
-    },
+  },
+  {
+    "vuki656/package-info.nvim",
+    config = true,
+  },
 }
